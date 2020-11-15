af412cf47ee9a7f2add56426d7fc31d168690ba1
Thomas <51972322+linn14@users.noreply.github.com>
2020-11-15 19:43:31 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const MPPClient = require("multiplayerpianojs");
-const mpp = new MPPClient();
+const mpp = new MPPClient()
+const config = require("./config.json");
+console.log("linn14/multiplayerpiano-bot");
+console.log("forked from dawoori/multiplayerpiano-bot");
 mpp.connect();
 mpp.on("connected", () => {
   console.log("Connected.");
-  mpp.setName("Sapphire | Build 1");
-  mpp.setChannel("Sapphire Bot").then(() => console.log("Channel Set!"));
+  mpp.setName(config.name);
+  mpp.setChannel(config.channel).then(() => console.log("Channel Set!"));
 });
 
 mpp.on("message", (msg) => {
@@ -19,14 +22,8 @@ const rl = readline.createInterface({
 
 rl.on("line", function (line) {
     const chars = line.split(' ');
-    const charsurl = line.split(' ');
   if (chars[0] === "play") {
     mpp.playMidi(__dirname + `/midi/${chars[1]}.mid`);
-  } else {
-    mpp.sendMessage(line);
-  }
-   if (chars[0] === "playurl") {
-    mpp.playMidi(`$charsurl`);
   } else {
     mpp.sendMessage(line);
   }
