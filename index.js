0c0e1c9bc4d685e55d852099f527576d5cd95dee
Thomas <51972322+linn14@users.noreply.github.com>
2020-11-15 19:08:35 +0000

    Progress coding

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const MPPClient = require("multiplayerpianojs");
 const mpp = new MPPClient();
-//const mpp = new MPPClient(SCOCKS/HTTPS PROXY HERE) alternatively
 mpp.connect();
 mpp.on("connected", () => {
-  console.log("bot connected");
-  mpp.setName("pianoman");
-  mpp.setChannel("channel").then(() => console.log("Channel Set!"));
+  console.log("Connected.");
+  mpp.setName("Sapphire | Build 1");
+  mpp.setChannel("Sapphire Bot").then(() => console.log("Channel Set!"));
 });
 
-// These are chat messages
 mpp.on("message", (msg) => {
   console.log(`${msg.user.name}: ${msg.content}`);
 });
@@ -21,8 +19,14 @@ const rl = readline.createInterface({
 
 rl.on("line", function (line) {
     const chars = line.split(' ');
-  if (chars[0] === "p") {
+    const charsurl = line.split(' ');
+  if (chars[0] === "play") {
     mpp.playMidi(__dirname + `/midi/${chars[1]}.mid`);
+  } else {
+    mpp.sendMessage(line);
+  }
+   if (chars[0] === "playurl") {
+    mpp.playMidi(`$`);
   } else {
     mpp.sendMessage(line);
   }
